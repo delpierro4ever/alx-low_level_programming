3cb523892de4e6799f79d498603dfa9512a630f0
delpierro4ever <delpierro4ever@gmail.com>
2023-03-22 07:03:38 -0700

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * The main program starts reading here
- * */
+ * main - This is the main entry point
+ * Return: Always 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
