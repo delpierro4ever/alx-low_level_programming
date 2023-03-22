73b5373d7e2a7ddd8a21194f0c90cb82110d2b1f
delpierro4ever <delpierro4ever@gmail.com>
2023-03-22 07:10:59 -0700

    Commiting 5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - This is the main entry point
  * Return: Always 0
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art.");
 	return (0);
 }
