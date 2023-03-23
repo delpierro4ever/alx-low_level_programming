5bf065fe273b0fdfdbe19e63ae4a17478b86f6b7
delpierro4ever <delpierro4ever@gmail.com>
2023-03-23 06:57:54 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ * Return: always zer0
+ */
+
+int main(void)
+{
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar("\n");
+	return (0);
+}
