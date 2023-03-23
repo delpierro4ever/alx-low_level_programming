6424f016a01d9791fca3986915529951d017c9b7
delpierro4ever <delpierro4ever@gmail.com>
2023-03-23 05:46:26 -0700

    commiting positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,10 +3,12 @@
 /* more headers goes there */
 #include <stdio.h>
 
-/* 
- * main - This is the entry point of the program
- * Return: returns 0
- * */
+/* betty style doc for function main goes there 
+ *
+ * main - main function starts here
+ * Return: always returns 0
+ *
+ */
 
 int main(void)
 {
@@ -15,17 +17,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n>0) 
-	{
-		printf("\n%d: is positive", n);
-	}
-	else if (n==0)
-	{
-		printf("\n%d: is zero", n);
-	}
-	else (n<0)
-	{
-		printf("\n%d: is negative", n);
-	}
+	if (n > 0)
+		printf("%d is postive", n);
+	else if (n == 0)
+		printf("%d is zero", n);
+	else 
+		printf("%d is negative", n);
+	printf("\n");
 	return (0);
 }
