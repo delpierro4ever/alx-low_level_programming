687b412232e80fe690a767408204558afc5a6c23
delpierro4ever <delpierro4ever@gmail.com>
2023-03-24 06:16:07 -0700

    commiting exercise nine

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - This is main entry point of the program
+ *
+ * Return: always zero
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	putchar('\n');
+	return (0);
+}
