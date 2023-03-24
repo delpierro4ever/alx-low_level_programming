1d3c918250ed56ffc3d7b4438573741aba405af7
delpierro4ever <delpierro4ever@gmail.com>
2023-03-24 07:08:00 -0700

    commiting exercise ten

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - main entry point of the program
+ * Return: always zero
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			if(i < j && i != j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+}
