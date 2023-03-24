2ba88dff7b6834b230a899c5e4f4457c031bc1a5
delpierro4ever <delpierro4ever@gmail.com>
2023-03-24 06:29:01 -0700

    commiting exercise nine

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,9 +12,12 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+		if (i == 9)
+			putchar(i + '0');
+		else
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
 	}
 	putchar('\n');
 	return (0);
