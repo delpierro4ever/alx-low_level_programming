9b036717078749a4f2d0bd2e852d23e337a28b84
delpierro4ever <delpierro4ever@gmail.com>
2023-03-24 05:19:04 -0700

    commiting exercise six

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main entry point of program
+ * Return: always zero
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar('i');
+	putchar('\n');
+	return (0);
+}
