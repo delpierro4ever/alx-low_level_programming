0075e4cd0b1d1a25ee4e26bcb1456ec1b54f2fc2
delpierro4ever <delpierro4ever@gmail.com>
2023-03-24 05:43:30 -0700

    commiting exercise eight

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always zero
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i + '0');
+	for (j = a; j <= f; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
