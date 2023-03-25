24f5c47f9790a456fa1577a539de8fc2bd066ec8
delpierro4ever <delpierro4ever@gmail.com>
2023-03-25 04:51:10 -0700

    commit function that prints alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet: print alphabet function
+ * Return: always void
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i == 'a'; i <= 'z'; i++)
+		_putchar(i);
+	putchar('\n');
+	return void;
+}
