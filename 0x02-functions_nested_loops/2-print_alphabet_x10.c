0a242e1858fc714e3b800dd63aaae1d55fc362bd
delpierro4ever <delpierro4ever@gmail.com>
2023-03-25 05:10:17 -0700

    commiting function that prints alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10(void) - main function prototype
+ * Return: always void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet(void);
+		_putchar('\n');
+	}
+}
