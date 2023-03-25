479adc6add386dec3fcd8197d2226ab0f1895b73
delpierro4ever <delpierro4ever@gmail.com>
2023-03-25 03:01:36 -0700

    commiting exercise 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - main Entry point of program
+ * Return: always zero
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0: i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(k + '0');
+					if (k + l != 18)
+						putchar(',');
+				}
+			}
+		}
+	}
+}
