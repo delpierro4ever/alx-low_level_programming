1a09d52ea8971cca03680e16a8d4ffaf31809832
delpierro4ever <delpierro4ever@gmail.com>
2023-03-27 02:57:07 -0700

    commiting exercise 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - the main name of the prototype
+ *
+ * c: parameter to be printed
+ *
+ * Return: returns either zero or one
+ */
+
+int _islower(int c);
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
