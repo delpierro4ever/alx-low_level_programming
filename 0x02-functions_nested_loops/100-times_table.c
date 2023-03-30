2c03c3309dc862306eece55e6cc48606977e01d3
delpierro4ever <delpierro4ever@gmail.com>
2023-03-30 06:50:26 -0700

    commiting time table function

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,7 +14,7 @@ void print_times_table(int n)
 	int j;
 	int k;
 
-	if (n !< 0 || n !> 15)
+	if (n !< 0 && n !> 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -24,12 +24,11 @@ void print_times_table(int n)
 
 				if (j != n)
 				{
-					printf("%d, ", k)
+					printf("%d, ", k);
 				}
 				else
-					prinf("%d\n", k);
+					printf("%d\n", k);
 			}
 		}
 	}
-	return (void);
 }
