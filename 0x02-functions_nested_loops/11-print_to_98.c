68e285bec0e49c864d48527b8c538461fdce1cda
delpierro4ever <delpierro4ever@gmail.com>
2023-03-30 05:09:08 -0700

    commiting function to print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - main entery of function
+ *
+ * @n: main parameter of function
+ */
+
+void print_to_98(int n);
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+		printf("%d, ", i);
+	_putchar('\n');
+	printf("98");
+}
