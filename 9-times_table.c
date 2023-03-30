b58d381337ffd639dc9adf47a97131580fc4ab65
delpierro4ever <delpierro4ever@gmail.com>
2023-03-30 04:25:24 -0700

    commiting corrections

--- 9-times_table.c
+++ 9-times_table.c
@@ -10,12 +10,18 @@ void times_table(void)
 {
 	int i;
 	int j;
+	int k;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j  = 0; j <= 9; j++)
 		{
-			k = i * j;
+			k = j * i;
+			if (j != 9)
+			{
+				printf("%d", k);
+				_putchar(', ');
+			}
 			printf("%d", k);
 			_putchar('\n');
 		}
