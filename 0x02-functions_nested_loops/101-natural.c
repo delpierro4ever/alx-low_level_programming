c54115c7a82d7f466ebefd1027c85ea40694f7aa
delpierro4ever <delpierro4ever@gmail.com>
2023-03-30 07:56:29 -0700

    Commiting program that prints multiple of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ * Retrun: always zero
+ */
+
+int main()
+{
+	int i;
+	int j = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			j = j + i;
+		}
+	}
+	printf("%d\n", j);
+}
