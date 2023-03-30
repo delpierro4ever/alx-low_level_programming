b8e2c402b09c0c8f1a49067bd2b380a6724202aa
delpierro4ever <delpierro4ever@gmail.com>
2023-03-30 05:59:51 -0700

    updating errors of prit to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,15 +10,30 @@
 void print_to_98(int n)
 {
 	int i;
-
-	for (i = n; i <= 98; i++)
+	if (i < 98)
 	{
-		if (i != 98)
-			printf("%d, ", i);
-		else
+		for (i = n; i <= 98; i++)
 		{
-			printf("%d", i);
-			_putchar('\n');
+			if (i != 98)
+				printf("%d, ", i);
+			else
+			{
+				printf("%d", i);
+				_putchar('\n');
+			}
 		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+                {
+                        if (i != 98)
+                                printf("%d, ", i);
+                        else
+                        {
+                                printf("%d", i);
+                                _putchar('\n');
+                        }
+                }
 	}
 }
