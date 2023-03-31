7cc05d5a1e28a180b5ea9871f12e5530c755b85a
delpierro4ever <delpierro4ever@gmail.com>
2023-03-30 17:38:27 -0700

    commiting changes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,16 +8,16 @@
 void jack_bauer(void)
 {
 	int a, b, c, d;
-	
+
 	for (a = 0; a <= 2; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			for(c = 0; c <= 5; c++)
+			for (c = 0; c <= 5; c++)
 			{
-				for(d = 0; d <= 9; d++)
+				for (d = 0; d <= 9; d++)
 				{
-					if (a >= 2 && b >=4)
+					if (a >= 2 && b >= 4)
 						break;
 					_putchar(a + 48);
 					_putchar(b + 48);
