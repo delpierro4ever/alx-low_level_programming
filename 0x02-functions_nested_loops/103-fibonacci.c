ff3ea8d1975fdf75ae50a0d91a52c0460ed1a92b
delpierro4ever <delpierro4ever@gmail.com>
2023-03-30 17:55:58 -0700

    comminting sum of even numbers of fib

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main entry of program
+ * Return: always zero
+ */
+
+int main(void)
+{
+	int a, i = 0, j = 1, k = 0, n;
+
+	for (n = 0; n <= 35; n++)
+	{
+		k = i + j;
+		if (k % 2 == 0 && k <= 4000000)
+			a = a + k;
+		else if (k > 4000000)
+			break;
+	}
+	printf("%d\n", a);
+	return (0);
+}
