54346747341191e77586105cf7104782b700ed07
delpierro4ever <delpierro4ever@gmail.com>
2023-04-03 16:50:46 -0700

    commiting things

---
+++ alx-low_level_programming/0x03-debugging/positive_or_negative.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - main entry
+ * Return: 0
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative", i);
+	else
+		printf("% is positive", i);
+}
